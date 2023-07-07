c87d2a403131d74442dad7111d9400030e5c9951
Yauheni <avegen.dr@gmail.com>
2023-07-07 22:40:05 +0300

    Corrects cors on the server

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -23,7 +23,7 @@ function AppComponent() {
 
   console.log(process.env);
 
-  const socket: any = io('wss://w3bstreaming.vara-network.io:3000/ws/');
+  const socket: any = io('wss://w3bstreaming.vara-network.io');
 
   return (
     <div className={cx(styles['app-container'])}>
--- packages/signaling-server/src/server.ts
+++ packages/signaling-server/src/server.ts
@@ -17,7 +17,7 @@ import {
 const app = express();
 app.use(cors());
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin');
+  res.header('Access-Control-Allow-Origin', '*');
   next();
 });
 
