0344eda83a4e179ea3e96852076ae6492cac53c4
Yauheni <avegen.dr@gmail.com>
2023-07-09 00:37:58 +0300

    Corrects port

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -17,10 +17,10 @@ function AppComponent() {
   const { isAccountReady } = useAccount();
 
   const isAppReady = isApiReady && isAccountReady;
-  const address = process.env.REACT_APP_SIGNALING_SERVER || 'http://127.0.0.1:3001';
+  const address = process.env.REACT_APP_SIGNALING_SERVER || 'http://127.0.0.1:3001'; //https://w3bstreaming-backend.vara-network.io/
 
   // const socket: any = io(`${address}/`);
-  const socket: any = io(`https://w3bstreaming-backend.vara-network.io/`);
+  const socket: any = io('https://w3bstreaming-backend.vara-network.io/');
 
   return (
     <div className={cx(styles['app-container'])}>
