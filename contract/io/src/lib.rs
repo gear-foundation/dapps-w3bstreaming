b224d863b3142b5edcc482965ec9d17a067c218c
Louisa <30317856+LouiseMedova@users.noreply.github.com>
2023-07-12 04:02:28 +0300

    Update deps (#9)
    * updated state according to design
    
    * updated deps
    
    * fixed contract

--- contract/io/src/lib.rs
+++ contract/io/src/lib.rs
@@ -8,31 +8,67 @@ use scale_info::TypeInfo;
 #[derive(Encode, Decode, TypeInfo, Clone)]
 pub struct Stream {
     pub broadcaster: ActorId,
-    pub timestamp: u64,
+    pub start_time: u64,
+    pub end_time: u64,
     pub title: String,
+    pub img_link: String,
     pub description: Option<String>,
     pub watchers: Vec<ActorId>,
+}
+
+#[derive(Encode, Decode, TypeInfo, Clone)]
+pub struct Profile {
+    pub name: Option<String>,
+    pub surname: Option<String>,
+    pub img_link: Option<String>,
+    pub stream_ids: Vec<String>,
+    pub subscribers: Vec<ActorId>,
+    pub subscriptions: Vec<Subscription>,
+    pub role: Role,
+}
+
+#[derive(Encode, Decode, Default, TypeInfo)]
+pub struct Contract {
+    pub streams: BTreeMap<String, Stream>,
+    pub users: BTreeMap<ActorId, Profile>,
+}
+
+#[derive(Encode, Decode, TypeInfo, Clone)]
+pub enum Role {
+    Speaker,
+}
+
+#[derive(Encode, Decode, TypeInfo, Clone)]
+pub struct Subscription {
+    pub account_id: ActorId,
+    pub sub_date: u64,
+    pub next_write_off: Option<u64>,
 }
 
 #[derive(Encode, Decode, TypeInfo)]
 pub enum Action {
     NewStream {
-        timestamp: u64,
         title: String,
         description: Option<String>,
+        start_time: u64,
+        end_time: u64,
+        img_link: String,
     },
-    SubscribeToStream {
-        id: String,
+    Subscribe {
+        account_id: ActorId,
     },
-    FinishStream {
-        id: String,
+    EditProfile {
+        name: Option<String>,
+        surname: Option<String>,
+        img_link: Option<String>,
     },
 }
 
 #[derive(Encode, Decode, TypeInfo)]
 pub enum ActionResult {
     StreamIsScheduled { id: String },
-    Subscribed { id: String },
+    Subscribed,
+    ProfileEdited,
     StreamIsFinished { id: String },
     Error(String),
 }
@@ -45,5 +81,5 @@ impl Metadata for ProgramMetadata {
     type Reply = InOut<(), ()>;
     type Others = InOut<(), ()>;
     type Signal = ();
-    type State = BTreeMap<String, Stream>;
+    type State = Contract;
 }
